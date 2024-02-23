7b23cc4cda7e632bb2984b38b819765042a1c522
Karl <karl_dilma@hotmail.com>
2024-02-23 17:21:23 +1100

    feat: scss init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from "next/app";
+import RootLayout from "./layout";
+
+import "@/design-system/index.scss";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <RootLayout>
+      <Component {...pageProps} />
+    </RootLayout>
+  );
+}
