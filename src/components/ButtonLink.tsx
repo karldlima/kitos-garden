e705c30ff461e3f49aff6a52f8ae4a9eaa5d4b27
Karl <karl_dilma@hotmail.com>
2024-02-28 18:38:02 +1100

    feat: buttonLink component

---
+++ src/components/ButtonLink.tsx
@@ -0,0 +1,30 @@
+import type { ReactNode } from "react";
+import NextLink from "next/link";
+import classNames from "classnames";
+
+interface LinkProps {
+  children: ReactNode;
+  url: string;
+  blank?: boolean;
+  active?: boolean;
+  className?: string;
+}
+
+export const ButtonLink = ({
+  children,
+  url,
+  blank = false,
+  active = false,
+  className = undefined,
+}: LinkProps): JSX.Element => (
+  <NextLink
+    className={classNames(
+      className ||
+        "rounded w-fit inline-flex items-center cursor-pointer text-white bg-primaryBrand hover:bg-highlight px-4 py-2 font-semibold mt-6 md:mt-0 "
+    )}
+    href={url}
+    target={blank ? "_blank" : "_self"}
+  >
+    {children}
+  </NextLink>
+);
