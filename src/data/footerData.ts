0bab8183667f548f4685da02b28d0e3661be46b6
Karl <karl_dilma@hotmail.com>
2024-02-29 23:32:46 +1300

    fix: footer home link

--- src/data/footerData.ts
+++ src/data/footerData.ts
@@ -1,10 +1,11 @@
 import { Anchor } from "@/data/index";
 
 export const footerData: Anchor[] = [
-  { uid: 1, link: { text: "Posts", url: "/posts" } },
+  { uid: 1, link: { text: "Home", url: "/" } },
+  { uid: 2, link: { text: "Posts", url: "/posts" } },
   {
-    uid: 2,
+    uid: 3,
     link: { text: "Projects", url: "/projects" },
   },
-  { uid: 3, link: { text: "Contact", url: "/contact" } },
+  { uid: 4, link: { text: "Contact", url: "/contact" } },
 ];
