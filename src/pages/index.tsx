b494aac6eb035e2a30630cc604356eba9acaa89e
Karl <karl_dilma@hotmail.com>
2024-03-04 13:27:51 +1300

    feat: content and styling for landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from "next";
 import Image from "next/image";
+import Link from "next/link";
 
 import { getEntry } from "../content/provider";
 
@@ -9,22 +10,21 @@ export interface PageProps {
 
 export default function Page({ homeData }: PageProps) {
   return (
-    <div className="mb-20">
-      <section className="bg-white dark:bg-secondary">
-        {/* TODO: review all tw stylings */}
-        <div className="grid max-w-screen-xl ml-auto lg:gap-8 xl:gap-0 lg:grid-cols-12">
-          <div className="mr-auto px-16 py-14 place-self-center lg:col-span-5">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+    <div className="mb-16 md:mb-20">
+      <section className="bg-secondary">
+        <div className="grid ml-auto xl:gap-0 md:grid-cols-12">
+          <div className="order-2 mr-auto px-10 py-12 md:order-1 md:pb-0 md:px-12 md:pt-14 xl:px-16 xl:pt-18 md:col-span-5">
+            <h1 className="max-w-2xl mb-4 text-3xl font-extrabold tracking-tight leading-none md:text-xl lg:text-2xl xl:text-6xl text-white">
               Kia Ora, I'm Karl
             </h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            <p className="max-w-2xl mb-6 font-light text-secondary text-lg md:mb-8 md:text-base lg:text-xl">
               I'm a Frontend Software Developer and this is my blog/project
               portfolio/digital garden of random software development tips and
               thoughts
             </p>
-            <a
-              href="#"
-              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            <Link
+              href="#about-me"
+              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white"
             >
               A bit about me
               <svg
@@ -39,10 +39,11 @@ export default function Page({ homeData }: PageProps) {
                   clip-rule="evenodd"
                 ></path>
               </svg>
-            </a>
+            </Link>
           </div>
-          <div className="hidden w-fit h-fit lg:mt-0 lg:col-span-7 lg:flex">
+          <div className="order-1 w-fit h-fit md:mt-0 md:col-span-7 md:flex md:order-2">
             <Image
+              className="md:min-w-[580px] md:min-h-[356px]"
               src="/assets/hero/cafe.png"
               alt="mockup"
               width={2000}
@@ -52,26 +53,30 @@ export default function Page({ homeData }: PageProps) {
           </div>
         </div>
       </section>
-      <section className="bg-primary">
-        <p className="mt-28 w-3/5 mx-auto leading-6">
+      <section id="about-me" className="bg-primary">
+        <p className="mt-14 w-4/5 mx-auto leading-6 md:w-3/5 md:mt-28">
           I'm Karl. A software developer that enjoys creating reliable customer
           facing solutions for growth focused business'. Here's a little about
           me and what I do for work:
         </p>
-        <h3 className="mt-10 w-3/5 mx-auto">About Me</h3>
-        <p className="mt-4 w-3/5 mx-auto leading-6">
-          Fuelled by curiosity and an open mind, I found my way to Melbourne,
-          blending my knack for problem-solving with a software engineering
-          career. With 6+ years of experience, I'm primarily focused on using
-          React/Redux, JavaScript, testing libraries and analytics to build
-          scalable reliable customer facing solutions for products and brands in
-          various domains such as Telecom, Consulting Services, Banking and Car
-          Rental.
+        <h3 className="mt-6 w-4/5 md:mt-10 md:w-3/5 mx-auto">About Me</h3>
+        <p className="mt-2 w-4/5 md:mt-4 md:w-3/5 mx-auto leading-6">
+          Fuelled by curiosity, I found my way to Melbourne, blending my knack
+          for problem-solving with a software engineering career. With 6+ years
+          of experience, I'm primarily focused on using React/Redux, JavaScript,
+          testing libraries and analytics to build scalable solutions for
+          products and brands in various domains such as Telecom, Consulting
+          Services, Banking and Car Rental.
         </p>
-        <h3 className="mt-10 w-3/5 mx-auto">Beyond the Screen</h3>
-        <p className="mt-4 w-3/5 mx-auto">
-          Imigrating to New Zealand in the early 2000s, I enjoy chasing
-          adrenaline through
+        <h3 className="mt-6 w-4/5 md:mt-10 md:w-3/5 mx-auto">
+          Beyond the Screen
+        </h3>
+        <p className="mt-2 w-4/5 md:mt-4 md:w-3/5 mx-auto leading-6">
+          Having immigrated to New Zealand in the early 2000s, my upbringing in
+          India and subsequent life in NZ have instilled in me a profound
+          adaptability to change. Growing up between cultures, embracing an open
+          mind, I love connecting with people from all walks of life and thrive
+          on the diversity of the human experience.
         </p>
       </section>
     </div>
