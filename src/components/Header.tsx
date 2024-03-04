8089539bbda63b4751504213783de89e131d6e39
Karl <karl_dilma@hotmail.com>
2024-03-04 13:27:02 +1300

    feat: overlay header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,8 +13,8 @@ export const Header = (): JSX.Element => {
   const router = useRouter();
 
   return (
-    <header className="bg-primary border-b-2 sticky top-0">
-      <nav className="flex items-center justify-between flex-wrap p-4">
+    <header className="z-50 bg-primary border-b-2 sticky top-0">
+      <nav className="flex items-center justify-between flex-wrap md:p-4">
         <Link
           className="flex items-center flex-shrink-0 text-white mr-6 md:mr-72"
           href="/"
@@ -31,21 +31,23 @@ export const Header = (): JSX.Element => {
         <div className="block md:hidden">
           <button
             onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-            className="flex items-center px-3 py-2 rounded text-primary hover:text-secondary"
+            className="flex items-center px-3 py-2 rounded text-primary"
           >
             <svg
-              className={`fill-current h-3 w-3 ${
+              className={classNames(
+                "fill-current h-5 w-5",
                 mobileMenuOpen ? "hidden" : "block"
-              }`}
+              )}
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
             <svg
-              className={`fill-current h-3 w-3 ${
+              className={classNames(
+                "fill-current h-5 w-5",
                 mobileMenuOpen ? "block" : "hidden"
-              }`}
+              )}
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -54,9 +56,10 @@ export const Header = (): JSX.Element => {
           </button>
         </div>
         <div
-          className={`w-full block flex-grow pl-2.5 md:pl-0 md:flex md:items-center md:w-auto ${
+          className={classNames(
+            "absolute top-nav w-full opacity-90 block flex-grow pb-5 pl-5 bg-primary md:static md:p-0 md:flex md:items-center md:w-auto",
             mobileMenuOpen ? "block" : "hidden"
-          }`}
+          )}
         >
           <div className="text-sm md:flex-grow space-x-0 md:space-x-10">
             {headerData?.map(({ link, uid }) => {
@@ -85,9 +88,7 @@ export const Header = (): JSX.Element => {
               );
             })}
           </div>
-          <div>
-            <ButtonLink url="/contact">Contact</ButtonLink>
-          </div>
+          <ButtonLink url="/contact">Contact</ButtonLink>
         </div>
       </nav>
     </header>
