de7fad9984736e613431b7fb9b149110dea5b134
Karl <karl_dilma@hotmail.com>
2024-03-08 19:34:39 +1100

    feat: home page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
+import { HeroIcon } from "@/components/index";
 import { getEntry } from "../content/provider";
 
 export interface PageProps {
@@ -9,35 +10,28 @@ export interface PageProps {
 }
 
 export default function Page({ homeData }: PageProps) {
+  const { title, subtitle } = homeData?.data?.attributes;
   return (
     <div>
       <section className="bg-secondary">
         <div className="grid ml-auto xl:gap-0 md:grid-cols-12">
           <div className="order-2 mr-auto px-10 py-12 md:order-1 md:pb-0 md:px-12 md:pt-18 xl:px-16 xl:pt-22 md:col-span-5">
             <h1 className="max-w-2xl mb-4 text-3xl font-extrabold tracking-tight leading-none md:text-3xl lg:text-4xl xl:text-6xl text-white">
-              Kia Ora, I'm Karl
+              {title}
             </h1>
             <p className="max-w-2xl mb-6 font-light text-secondary text-lg md:mb-8 md:text-base lg:text-xl">
-              I'm a Frontend Software Developer and this is my digital garden of
-              projects and random software development tips & thoughts
+              {subtitle}
             </p>
+            {/* TODO: use ButtonLink and implement cva for customized display */}
             <Link
               href="#about-me"
-              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white"
+              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center rounded text-white"
             >
               A bit about me
-              <svg
-                className="w-5 h-5 ml-2 -mr-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
+              <HeroIcon
+                icon="arrow-right"
+                className="w-5 h-5 ml-2 text-white"
+              />
             </Link>
           </div>
           <div className="order-1 w-fit h-fit md:mt-0 md:col-span-7 md:flex md:order-2">
