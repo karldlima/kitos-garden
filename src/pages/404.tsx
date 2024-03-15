aab89c20a9453380ebdfb4973719dc605d1e462f
Karl <karl_dilma@hotmail.com>
2024-03-15 14:43:32 +1100

    feat: 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <div className="min-h-screen mx-auto my-8 md:my-16">
+      <h1 className="text-center">I probably haven't made this page yet</h1>
+    </div>
+  );
+}
