90710c73fb657c69dc35673f2dbe6ad4b80bb826
Karl <karl_dilma@hotmail.com>
2024-03-15 14:43:06 +1100

    feat: single project page

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,13 +1,61 @@
 import { GetStaticProps } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import Markdown from "react-markdown";
 
 import { getProjectIndex, getProject } from "@/content/helpers/index";
+import { getTechnologies } from "@/content/utils/index";
 
 export interface PageProps {
   project: any;
 }
 
 export default function SingleProjectPage({ project }: PageProps) {
-  return <div>Single Project</div>;
+  const { title, description, image, technologies, link } =
+    project?.data?.[0]?.attributes ?? {};
+  const techRefined = getTechnologies(technologies?.data).join(" • ");
+  return (
+    <>
+      <section className="text-center my-8 md:my-16">
+        <h1 className="mb-3 max-w-80 mx-auto">{title}</h1>
+      </section>
+      <section className="flex flex-col mx-auto w-8/12 md:w-6/12 min-h-screen">
+        <Image
+          className="w-[564px] h-fit self-center mb-8"
+          src={image?.data?.attributes?.formats?.large?.url}
+          alt={"TODO"}
+          width={800}
+          height={800}
+        />
+        <p className="mb-10">
+          <Markdown>{description}</Markdown>
+        </p>
+        <p className="flex text-primaryBrand mb-4 gap-4">
+          {link?.map((l) => {
+            return (
+              <Link
+                href={l?.url ?? "#"}
+                target="_blank"
+                rel="noopener"
+                className="text-black dark:text-primaryBrand hover:text-highlight ml-3"
+              >
+                <span className="sr-only">{l?.linkType}</span>
+                <Image
+                  className="h-6"
+                  src={`/assets/icons/${l?.linkType}.svg`}
+                  alt={`${l?.linkType} icon`}
+                  width={25}
+                  height={50}
+                  priority="high"
+                />
+              </Link>
+            );
+          })}
+        </p>
+        <p className="text-xl text-primaryBrand">{techRefined}</p>
+      </section>
+    </>
+  );
 }
 
 export async function getStaticPaths() {
